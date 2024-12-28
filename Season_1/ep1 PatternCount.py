7cbc4b63169c4e667cf648f979237c34427608f1
Mohit Joshi <110181960+mhtjsh@users.noreply.github.com>
2024-12-28 11:07:24 +0530

    Update ep1 PatternCount.py

--- Season_1/ep1 PatternCount.py
+++ Season_1/ep1 PatternCount.py
@@ -1,1 +1,9 @@
-
+text = 'ATGCATATGACTACTAGATACTGATACTGATACATA'
+pattern = 'ATA'
+def PatternCount(Text, Pattern):
+    count = 0
+    for i in range(len(Text)-len(Pattern)+1):
+        if Text[i:i+len(Pattern)] == Pattern:
+            count = count+1 
+    return count 
+print(PatternCount(text, pattern))
